0ca4f49ac32c92e0443d6a58e7a206d0f4c30302
Parth Bhagat <parthb@nyu.edu>
2023-03-13 18:27:00 -0700

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -7,4 +7,8 @@ unzip RNN_Swin_4.zip
 cd ../
 pip install -r requirements.txt
 #pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
-
+gcloud alpha storage cp gs://old-films/yb_full.zip
+unzip yb_full.zip 
+mv yb_full test_data/data_1/
+rm -rf test_data/data_1/002/ 
+mv test_data/data_1/yb_full test_data/data_1/002
