378f2b27894a23710ee62e32af08533700d92399
Parth Bhagat <parthb@nyu.edu>
2023-03-13 18:23:50 -0700

    edit setup env sh

--- setup_env.sh
+++ setup_env.sh
@@ -6,5 +6,5 @@ cd OUTPUT
 unzip RNN_Swin_4.zip
 cd ../
 pip install -r requirements.txt
-pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+#pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 
